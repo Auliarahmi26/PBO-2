f847a803c12eb34e9b8d4b205319f40ed5c64804
Auliarahmi26 <33126332+Auliarahmi26@users.noreply.github.com>
2017-10-26 23:33:52 -0700

    membuat METHOD cetakAttribute

--- src/pbo2/myFrame.java
+++ src/pbo2/myFrame.java
@@ -8,4 +8,11 @@ public class myFrame {
     String pesan;
     int bilBulat;
     double bilDesimal;
+    
+    void cetakAttribute(){
+        System.out.println("Isi dari pesan :"+pesan);
+        System.out.println("Isi dari bilBulat :"+bilBulat);
+        System.out.println("Isi dari bilDesimal :"+bilDesimal);
+        
+    }
 }
